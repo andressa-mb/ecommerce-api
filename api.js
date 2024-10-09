cf02891f33be1b34d12a6ff62928a29cc8df7aa8
andressa-mb <andressamesquita26@gmail.com>
2024-10-09 19:56:07 -0300

    Created http-status, env and improvements to API

--- api.js
+++ api.js
@@ -2,97 +2,122 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
-const connectBc = require('./connectDb');
+const dotenv = require('dotenv');
 const orderModel = require('./order-model');
+const httpStatus = require('./http-status');
 
+dotenv.config();
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-  console.log("GET");
-  
-  const orders = await orderModel.find({});
-  //ou .exec()
-  console.log(orders);
-  
-  return res.status(200).json(orders);
-  
+app.get('/', async (req, res) => {  
+  const orders = await orderModel.find({});  
+  return res.status(httpStatus.OK).json({
+    message: "Orders",
+    data: orders
+  });  
 })
 
 app.get('/order/:id', async (req, res) => {
-  const getId = req.params.id;
+  const {id} = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(getId)) {
-    console.error('ID inválido');
-    return res.status(404).json("id invalido");
-  } 
-
-  const existId = await orderModel.findById(getId);
-
-  if(!existId) {
-    console.log("ID não encontrado no banco");
-    return res.status(404).json("Id não encontrado");
+  try{
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(httpStatus.NOT_FOUND).json({
+        message: "Order ID is not valid."
+      });
+    }   
+    const foundOrder = await orderModel.findById(id);  
+    if(!foundOrder) {
+      return res.status(httpStatus.NOT_FOUND).json({
+        message: "Order ID not found."
+      });
+    }
+    return res.status(httpStatus.OK).json({
+      message: "Order ID found successfully.",
+      data: foundOrder
+    });
+  } catch(e){
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: "Error while getting order by id." + e
+    })
   }
-
-  console.log("ID encontrado: " + getId);
-  return res.status(200).json("ID encontrado " + getId);
-
 })
 
 app.post('/order', async (req, res) => {
   const data = req.body;
-  console.log(req.body);
 
-  const sendOrders = await orderModel.create(data);
+  try{
+    const createOrder = await orderModel.create(data);
     
-  return res.status(200).json(sendOrders);
-    
+    return res.status(httpStatus.OK).json({
+      message: "Order created successfully.",
+      data: createOrder
+    });
+  }catch(e){
+    return res.status(httpStatus.BAD_REQUEST).json({
+      message: "Error while created a new order. " + e
+    })
+  }   
 })
 
 app.put('/order/:id', async (req, res) => {
-  const getId = req.params.id;
+  const {id} = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(getId)) {
-    console.error('ID inválido');
-    return res.status(404).json("id invalido");
-  } 
-
-  console.log("iniciei a edição");
-  const order = await orderModel.findByIdAndUpdate({_id: getId }, req.body, {new: true});
-  console.log("olha o order");
-  console.log(order);
-
-  if(!order){
-    console.log("não encontrado");
-    return res.status(404).json("Não existe no banco");
+  try{
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(httpStatus.NOT_FOUND).json({
+        message: "Order ID is not valid."
+      });
+    }
+    const foundOrder = await orderModel.findByIdAndUpdate({_id: id }, req.body, {new: true});  
+    if(!foundOrder){
+      return res.status(httpStatus.NOT_FOUND).json({
+        message: "Order ID not found."
+      });
+    }
+    return res.status(httpStatus.OK).json({
+      message: "Order updated successfully.",
+      data: foundOrder
+    });
+  }catch(e){
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: "Error while edit order by ID." + e
+    })
   }
-
-  console.log("Deu tudo certo, olha o order");
-  return res.status(200).json(order);
 })
 
 app.delete('/order/:id', async (req, res) => {
-  const getId = req.params.id;
-  console.log("id a ser deletado: " + getId);
-  
-  
-if (!mongoose.Types.ObjectId.isValid(getId)) {
-  console.error('ID inválido');
-  return res.status(404).json("id invalido");
-} 
-
-const existId = await orderModel.findById(getId);
-
-if(!existId){
-  return res.status(404).json("id não existe no banco");
-}
-  await orderModel.deleteOne(existId)
-
-  console.log("ID que achou no find" + existId);
-  return res.status(200).json("ID excluído do banco: " + existId);
-  
-
+  const {id} = req.params;
+  try{
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(httpStatus.NOT_FOUND).json({
+        message: "Order ID not found."
+      });
+    }    
+    const foundOrder = await orderModel.findById(id);
+    
+    if(!foundOrder){
+      return res.status(httpStatus.NOT_FOUND).json({
+        message: "Order ID not found."
+      });
+    }
+    await orderModel.deleteOne(foundOrder);    
+    return res.status(httpStatus.OK).json({
+      message: "Order deleted successfully.",
+      data: foundOrder
+    }); 
+  }catch(e){
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: "Error while deleting order ID."
+    })
+  }
 })
 
-app.listen(port, () => {
-    console.log(`Funcionando na porta ${port}`);
+app.listen(port, async () => {
+    try{
+      await mongoose.connect(process.env.DB_URL);
+      console.log(`Listening at: http://localhost:${port}`);
+    } catch(e){
+      console.log("Failed to connect. " + e);
+    }
 });
---
+++ http-status.js
@@ -0,0 +1,7 @@
+module.exports = {
+    INTERNAL_SERVER_ERROR: 500,
+    NOT_FOUND: 404,
+    BAD_REQUEST: 400,
+    OK: 200,
+    CREATED: 201
+}
