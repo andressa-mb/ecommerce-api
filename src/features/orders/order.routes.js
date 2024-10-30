b13ec00f1356c8d5851a41a96810b3aee47e67f8
Andressa Mesquita <81587338+andressa-mb@users.noreply.github.com>
2024-10-30 11:07:08 -0300

    Refactor delete order

--- src/features/orders/order.routes.js
+++ src/features/orders/order.routes.js
@@ -6,5 +6,6 @@ router.post('/', orderController.createOrder);
 router.get('/', orderController.getOrders);
 router.get('/:id', orderController.getOrderById);
 router.put('/:id', orderController.updateOrder);
+router.delete('/:id', orderController.deleteOrder);
 
 module.exports = router;
