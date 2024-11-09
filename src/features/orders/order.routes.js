e006c9cfb669c0499b104b0422dd40c4a9b02308
andressa-mb <andressamesquita26@gmail.com>
2024-11-09 16:59:15 -0300

    teste8

--- src/features/orders/order.routes.js
+++ src/features/orders/order.routes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const orderController = require('./order.controller');
 
 router.post('/', orderController.createOrder);
-router.get('/', (req, res, next) => {
-    console.log('Chamou a rota de obtenção de pedidos!');
-    orderController.getOrders(req,res,next)
-});
+router.get('/', orderController.getOrders);
 router.get('/:id', orderController.getOrderById);
 router.put('/:id', orderController.updateOrder);
 router.delete('/:id', orderController.deleteOrder);
