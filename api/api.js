9e6397c26f8e82223fcd587c690999243965512c
andressa-mb <andressamesquita26@gmail.com>
2024-11-09 23:27:04 -0300

    new teste3

--- api/api.js
+++ api/api.js
@@ -4,7 +4,6 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 //const helmet = require('helmet');
 const orderRoutes = require('../src/features/orders/order.routes');
-const orderModel = require('../src/features/orders/order.model');
 
 const app = express();
 const port = 3000;
@@ -12,7 +11,7 @@ const port = 3000;
 dotenv.config();
 app.use(express.json());
 app.use(cors());
-//app.use('/orders', orderRoutes);
+app.use('/orders', orderRoutes);
 
 // Configuração geral de segurança com helmet
 /* app.use(helmet.contentSecurityPolicy({
@@ -31,34 +30,10 @@ app.get('/ping', (req, res) => {
   })
 })
 
-app.get('/orders', (req, res) => {
-  res.status(200).json({ message: 'Rota /orders acessível!' });
-});
-
-app.get('/check-env', (req, res) => {
-  res.status(200).json({ dbUrl: process.env.DB_URL || "Variable not set" });
-});
-
-app.get('/test-query', async (req, res) => {
-  try {
-    const orders = await orderModel.find().limit(10).exec();
-    console.log(orders);
-    res.status(200).json(orders);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Erro de consulta!' });
-  }
-});
-
-app.get('/test-connection', async (req, res) => {
-  try {
-    await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-    res.status(200).json({ message: 'Conexão bem-sucedida com o MongoDB!' });
-  } catch (error) {
-    console.error('Erro ao conectar:', error);
-    res.status(500).json({ message: 'Erro de conexão', error });
-  }
-});
+app.use('/orders', (req, res, next) => {
+  console.log("Rota /orders foi chamada");
+  next();
+}, orderRoutes);
 
 app.listen(port, async () => {
     try{
@@ -72,4 +47,3 @@ app.listen(port, async () => {
       console.log(`Failed to connect. Error: ${e}`);
     }
 });
-
